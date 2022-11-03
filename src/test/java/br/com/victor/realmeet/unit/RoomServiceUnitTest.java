9302bbd2dcb4689d290a8cde7086a5537d202933
VictorSimiao <victor.sreis@hotmail.com>
2022-11-03 20:42:03 -0300

    Teste unitario: Busca por uma Room que nnao existe

--- src/test/java/br/com/victor/realmeet/unit/RoomServiceUnitTest.java
+++ src/test/java/br/com/victor/realmeet/unit/RoomServiceUnitTest.java
@@ -4,16 +4,19 @@ import br.com.victor.realmeet.core.BaseUnitTest;
 import br.com.victor.realmeet.domain.entity.Room;
 import br.com.victor.realmeet.domain.repository.RoomRepository;
 import br.com.victor.realmeet.dto.response.RoomResponse;
+import br.com.victor.realmeet.exception.RoomNotFoundException;
 import br.com.victor.realmeet.service.RoomService;
 import br.com.victor.realmeet.utils.MapperUtils;
 import br.com.victor.realmeet.utils.TestConstants;
 import br.com.victor.realmeet.utils.TestDataCreator;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
@@ -30,14 +33,21 @@ public class RoomServiceUnitTest extends BaseUnitTest {
     }
 
     @Test
-    void testGetRoom(){
+    void testGetRoom() {
         Room room = TestDataCreator.newRoomBuilder().id(TestConstants.DEFAULT_ROOM_ID).build();
-        when(roomRepository.findById(TestConstants.DEFAULT_ROOM_ID)).thenReturn(Optional.of(room));
+        when(roomRepository.findByIdAndActive(TestConstants.DEFAULT_ROOM_ID, true)).thenReturn(Optional.of(room));
 
         RoomResponse roomResponse = roomService.getRoom(TestConstants.DEFAULT_ROOM_ID);
 
-        assertEquals(room.getName(),roomResponse.getName());
-        assertEquals(room.getSeats(),roomResponse.getSeats());
+        assertEquals(room.getName(), roomResponse.getName());
+        assertEquals(room.getSeats(), roomResponse.getSeats());
 
+    }
+
+
+    @Test
+    void testGetRoomNotFound() {
+        when(roomRepository.findByIdAndActive(TestConstants.DEFAULT_ROOM_ID, true)).thenReturn(Optional.empty());
+        assertThrows(RoomNotFoundException.class, () -> roomService.getRoom(TestConstants.DEFAULT_ROOM_ID));
     }
 }
