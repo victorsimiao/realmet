16573bf6f3af6669a2f43b2b5ff91884b8c24cee
Victor Reis <victor.sreis@hotmail.com>
2023-01-09 11:26:13 -0300

    Adiciona Docker

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM maven:3.8.5-openjdk-17 as build
+WORKDIR /build
+COPY . .
+RUN mvn clean package -DskipTests
+
+FROM openjdk:17
+WORKDIR /app
+COPY --from=build ./build/target/*.jar ./realmeet.jar
+ENTRYPOINT java -jar realmeet.jar
+
+
--- src/main/java/br/com/victor/realmeet/config/OpenAPIConfig.java
+++ src/main/java/br/com/victor/realmeet/config/OpenAPIConfig.java
@@ -2,7 +2,9 @@ package br.com.victor.realmeet.config;
 
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,10 +20,25 @@ public class OpenAPIConfig {
                                 .title("API Realmeet")
                                 .version("v0.0.1")
                                 .description("API fornece o agendamento e controle de reserva de salas.")
-                ).externalDocs(
+                                .contact(descriptionContact())
+                                .license(descriptionLicense())
+                )
+                .externalDocs(
                         new ExternalDocumentation().description("Documentação")
                 );
 
+    }
+
+    private Contact descriptionContact() {
+        return new Contact()
+                .name("Victor Simião")
+                .email("victor.sreis@hotmail.com");
+    }
+
+    private License descriptionLicense() {
+        return new License()
+                .name("License")
+                .url("#");
     }
 
 
