21f726fd596a0a453e320013de75fa2158a0d8a9
Victor Reis <victor.sreis@hotmail.com>
2023-01-18 09:50:57 -0300

    inTeste de integracao EmailConfigProperties

--- src/main/java/br/com/victor/realmeet/RealmeetApplication.java
+++ src/main/java/br/com/victor/realmeet/RealmeetApplication.java
@@ -2,8 +2,10 @@ package br.com.victor.realmeet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan("br.com.victor.realmeet.config.propeties")
 public class RealmeetApplication {
 
 	public static void main(String[] args) {
